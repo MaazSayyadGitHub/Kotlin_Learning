f9876ef6d793b03335016ad62d5238288613b08a
Maaz_Sayyad <maaz.s@peocitsoft.com>
2024-10-29 12:28:46 +0530

    val and var

---
+++ app/src/main/java/com/example/kotlin_learning/var_and_val.kt
@@ -0,0 +1,15 @@
+package com.example.kotlin_learning
+
+fun main() {
+    var myVariable : Int = 10
+    myVariable = 23
+    println("The value of our variable : $myVariable")
+
+    val myVal : String = "Maaz_Sayyad"
+    println("the val value is : $myVal")
+
+    val myAge = 25
+    val myFavNo = 10
+    println("my Age is : $myAge")
+    println("my FavNo is : $myFavNo")
+}
