ec5998f89c8874bf5b631eb037b7ea662fa790a1
Maaz_Sayyad <maaz.s@peocitsoft.com>
2024-10-29 18:44:17 +0530

    operators changes.

--- app/src/main/java/com/example/kotlin_learning/Operators.kt
+++ app/src/main/java/com/example/kotlin_learning/Operators.kt
@@ -14,6 +14,24 @@ fun main() {
     println("x + y : ${x + y}")
     println("x + y + z : ${x + y + z}")
 
+    var a : Double = 20.0
+    var b : Double = 8.0
 
+    println("The result is ${a / b}")
+
+    // Formula of Sphere
+    var radius = 5.5
+    var sphere = 0.75 * Math.PI * (radius*radius*radius)
+    println("The volume os sphere : $sphere")
+
+    // Strings
+    var name = "Maaz Sayyad"
+    println("This is my name : ${name[0]}")
+
+    // Comparison Operators
+    var p = 10
+    var q = 11
+    var r = 12
+    println(p < q || p > r)
 
 }
