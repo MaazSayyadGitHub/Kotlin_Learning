279de11a3cd390d2ba7f8f5d292787dde4eea929
Maaz_Sayyad <maaz.s@peocitsoft.com>
2024-10-30 11:24:49 +0530

    operators changes.

--- app/src/main/java/com/example/kotlin_learning/Operators.kt
+++ app/src/main/java/com/example/kotlin_learning/Operators.kt
@@ -1,18 +1,18 @@
 package com.example.kotlin_learning
 
 fun main() {
-    var x = 3
-    var y = 4
-    var z = 5
+//    var x = 3
+//    var y = 4
+//    var z = 5
 //    var result = x+y
 //    println("The result is : $result")
 //
 //    // or directly
 //    println(15 % 7)
 
-    println("x : $x")
-    println("x + y : ${x + y}")
-    println("x + y + z : ${x + y + z}")
+//    println("x : $x")
+//    println("x + y : ${x + y}")
+//    println("x + y + z : ${x + y + z}")
 
     var a : Double = 20.0
     var b : Double = 8.0
@@ -26,12 +26,21 @@ fun main() {
 
     // Strings
     var name = "Maaz Sayyad"
-    println("This is my name : ${name[0]}")
+    println("This is my name : ${name}")
 
     // Comparison Operators
-    var p = 10
-    var q = 11
-    var r = 12
-    println(p < q || p > r)
+//    var p = 10
+//    var q = 10
+//    var r = 12
+//    println(p != q)
 
+    val expression = (3 > 4 || 4 > 3) && 4 <= 4
+    println(expression)
+
+    val bool = true
+    val x = 9
+    val y = 3
+    val z = 9
+    val expression1 = !(x != z) && bool || z > (x + y) && (!bool || y < z)
+    println("expression1 - $expression1" ) // true
 }
